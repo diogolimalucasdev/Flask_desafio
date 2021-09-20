6640f03755394b22aa1b0c220418714dcb47cfca
Diogo Lima Lucas <66488127+diogolimalucasdev@users.noreply.github.com>
2021-09-20 10:52:02 -0300

    Adicionando o DELETE

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def registro():
 
 
 # aqui eu retorno a tarefa individual chamada por cada ID
-@app.route('/registro/<int:id>/', methods=['GET', 'PUT'])
+@app.route('/registro/<int:id>/', methods=['GET', 'PUT', 'POST'])
 def registro_pessoal(id):
     if request.method == 'GET':
         tarefas_pessoal = tarefas[id - 1]
@@ -62,11 +62,30 @@ def registro_pessoal(id):
                 lista = identificador, responsavel, tarefa
                 print(lista[0])
                 if dados['id'] == lista[0] and dados['responsavel'] == lista[1] and dados['tarefa'] == lista[2]:
-                    tarefas[id-1] = dados
+                    tarefas[id - 1] = dados
                     print(tarefas)
                     return jsonify(dados)
                 else:
                     print("nao foi")
+
+            else:
+                return "Login nao identificado"
+    elif request.method == 'POST':
+        dados = json.loads(request.data)
+
+        for valor in tarefas:
+            identificador, responsavel, tarefa, status = valor.values()
+
+            if id == identificador:
+                return "Id ja cadastrado"
+            else:
+                return tarefas.append(dados)
+
+
+@app.route('/registro/<int:id>/deletar/', methods=['DELETE'])
+def deletar(id):
+    tarefas.pop(id)
+    return jsonify({'status': 'sucesso', 'mensagem': 'Tarefa excluida'})
 
 
 if __name__ == '__main__':
